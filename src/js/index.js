9bf43856dd192e79ee28b7332fbdc88b9f3967a3
IonDuluta <130406100+IonDuluta@users.noreply.github.com>
2024-06-05 08:07:29 +0200

    fifth commit

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ function navigateTo(page) {
     //cambiare url mostrato
     const main = document.querySelector('main')
     const sections = main.querySelectorAll('section')
-    console.log(sections);
+    history.pushState({ page }, null, page);
 
     for (let section of sections) {
         if (section.id === page) {
@@ -22,3 +22,46 @@ function navigateTo(page) {
         // }
     }
 }
+
+function handleScrolltop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+const sezione = document.getElementById("pagina-scroll")
+const tornaSU = document.getElementById("torna-su")
+
+window.addEventListener('scroll', function () {
+    const altezzaSezione = sezione.scrollHeight;
+    const posizioneScroll = window.scrollY || window.pageYOffset ;
+
+    const percentualeScroll = (posizioneScroll / altezzaSezione) * 100
+
+    console.log(percentualeScroll);
+
+    if (percentualeScroll >= 60) {
+        tornaSU.classList.remove("hidden")
+    }else{
+        tornaSU.classList.add("hidden")
+
+    }
+})
+
+
+fetch('https://dog.ceo/api/breeds/image/random/40')
+    .then(response => response.json())
+    .then((data) => {
+        const paginaApi = document.getElementById("pagina-api")
+        data.message.forEach(imageSrc => {
+            const divContainer = document.createElement("div")
+            const img = document.createElement("img")
+            img.className = "h-auto max-w-full rounded-lg"
+            img.src = imageSrc
+            divContainer.appendChild(img)
+            paginaApi.appendChild(divContainer)
+        });
+
+
+    })
+
