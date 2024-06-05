b42757f8cde8421f8d07a073ce156f6c41d3c7ad
IonDuluta <130406100+IonDuluta@users.noreply.github.com>
2024-06-05 09:55:24 +0200

    sixth commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,15 @@
+const sezione = document.getElementById("pagina-scroll")
+const tornaSU = document.getElementById("torna-su")
+let currentPage = 'pagina-api'
+
 function navigateTo(page) {
-    //mostrare contenuto corretto
-    //cambiare url mostrato
+    
     const main = document.querySelector('main')
     const sections = main.querySelectorAll('section')
-    history.pushState({ page }, null, page);
+    history.pushState({
+        page
+    }, null, page);
+    currentPage = page;
 
     for (let section of sections) {
         if (section.id === page) {
@@ -11,15 +17,7 @@ function navigateTo(page) {
         } else {
             section.classList.add("hidden")
         }
-        // case "api":
-        //     break
-        // case "scroll":
-        //     break
-        // case "modulo":
-        //     break
-        // default:
-        //     break
-        // }
+
     }
 }
 
@@ -29,25 +27,23 @@ function handleScrolltop() {
         behavior: "smooth"
     })
 }
-const sezione = document.getElementById("pagina-scroll")
-const tornaSU = document.getElementById("torna-su")
 
 window.addEventListener('scroll', function () {
-    const altezzaSezione = sezione.scrollHeight;
-    const posizioneScroll = window.scrollY || window.pageYOffset ;
+    if (currentPage == 'pagina-scroll') {
+        const altezzaSezione = sezione.scrollHeight;
+        const posizioneScroll = window.scrollY || window.pageYOffset;
 
-    const percentualeScroll = (posizioneScroll / altezzaSezione) * 100
+        const percentualeScroll = (posizioneScroll / altezzaSezione) * 100
 
-    console.log(percentualeScroll);
+        console.log(percentualeScroll);
 
-    if (percentualeScroll >= 60) {
-        tornaSU.classList.remove("hidden")
-    }else{
-        tornaSU.classList.add("hidden")
-
+        if (percentualeScroll >= 40) {
+            tornaSU.classList.remove("hidden")
+        } else {
+            tornaSU.classList.add("hidden")
+        }
     }
 })
-
 
 fetch('https://dog.ceo/api/breeds/image/random/40')
     .then(response => response.json())
@@ -61,7 +57,39 @@ fetch('https://dog.ceo/api/breeds/image/random/40')
             divContainer.appendChild(img)
             paginaApi.appendChild(divContainer)
         });
-
-
     })
+
+const form = document.querySelector("form")
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const emailInput = document.getElementById("email")
+    const passwordInput = document.getElementById("password")
+    const confirmPasswordInput = document.getElementById("confirm-password")
+    const newsletterInput = document.getElementById("newsletter")
+    
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if(!emailRegex.test(emailInput.value)){
+        console.log("email non valida")
+        return
+    }
+    const passwordRegex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-zA-Z]).{8,}$/;
+    if(!passwordRegex.test(passwordInput.value)){
+        console.log("la password non rispeta i limiti")
+        return
+    }
+
+    if(passwordInput.value !== confirmPasswordInput.value){
+        console.log("le password non corrispondono")
+        return
+    }
+
+    if(!newsletterInput.checked){
+        console.log("acceta la newsletter");
+        return
+    }
+
+    console.log("well done");
+})
+
 
